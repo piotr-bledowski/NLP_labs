be6cac68cfa0d10ede3c54fdb14f4cafbc1ee944
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2025-02-01 20:02:52 +0100

    Cleaned up zipf.py

--- proj1/zipf.py
+++ proj1/zipf.py
@@ -1,17 +1,12 @@
 import pandas as pd
 
-# Read the file into a dataframe with column names
 df = pd.read_csv('proj1/words.txt', sep='\t', header=None, 
                  names=['Index', 'Word', 'Frequency'])
 
-# Decrease the index column by 100
 df['Index'] = df['Index'] - 100
 
-# Calculate Zipf score (frequency / index)
 df['Zipf_score'] = df['Frequency'] / df['Index']
 
-# Display first 10 results
 print(df.head(10))
 
-# Save to CSV file with headers
 df.to_csv('proj1/zipf_results.csv', index=False)
